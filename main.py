570f76eb850948d5148c654418567e605b054933
Milo Gertjejansen <milo@milogert.com>
2019-06-01 09:57:44 -0500

    Updated with Black and ic

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
-import sys
-from pprint import pprint
 
 try:
     import requests
 except ImportError:
     print("Failed to import requests")
-    sys.exit(1)
 
+try:
+    from icecream import ic
+except ImportError:
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)
+
+import sys
+from pprint import pprint
+import re
+import json
+import xml.etree.ElementTree as ET
+
+
+GAME_URL = "https://boardgamegeek.com/boardgame/{id}/"
 
 if len(sys.argv) < 2:
     print(f"Need a username, found {sys.argv}")
@@ -18,109 +28,108 @@ username = sys.argv[1]
 
 url = f"https://boardgamegeek.com//xmlapi2/collection?username={username}&trade=1"
 
-def tryforrequest(req_url):
+
+def do_get(req_url):
     resp = requests.get(req_url)
     if resp.status_code != 200:
+        ic("trying another request")
         import time
+
         time.sleep(1)
-        return tryforrequest(req_url)
+        return do_get(req_url)
 
     return resp.text
 
-out = tryforrequest(url)
 
-import xml.etree.ElementTree as ET
-
-parsed = ET.fromstring(out)
-
-formaturl = 'https://boardgamegeek.com/boardgame/{id}/'
-formatlink = '[{name}]({url})'
-formatrow = '{link}|{condition}|{price}|{notes}'
-
-gamerow = {}
+parsed = ET.fromstring(do_get(url))
 
 
-def getlink(item):
+def extract_link(item) -> str:
     name = item[0].text
     attrs = item.attrib
-    url = formaturl.format(id=attrs["objectid"])
-    return formatlink.format(name=name,
-                             url=url)
+    url = GAME_URL.format(id=attrs["objectid"])
+    return f"[{name}]({url})"
 
 
 def parse_metadata(item) -> dict:
     name = item[0].text
-    conditiontext = item.find('conditiontext')
+    conditiontext = item.find("conditiontext")
     if conditiontext is not None:
-        import re
-        jsonstring = re.search(r'bggtrade2reddit:(.*)', conditiontext.text)
+        jsonstring = re.search(r"bggtrade2reddit:(.*)", conditiontext.text)
         if jsonstring:
             metadata = jsonstring.group(1)
-            import json
             return json.loads(metadata)
 
     return {}
-    
+
 
 def should_ignore(item) -> bool:
-    return parse_metadata(item).get('ignore', False)
+    return parse_metadata(item).get("ignore", False)
 
 
-def getcondition(item):
+def get_condition(item) -> int:
     metadata = parse_metadata(item)
-    if 'condition' in metadata:
-        return metadata['condition']
+    if "condition" in metadata:
+        return metadata["condition"]
 
     return -1
 
 
 def get_price(item) -> str:
     metadata = parse_metadata(item)
-    if 'price' in metadata:
-        return metadata['price']
+    if "price" in metadata:
+        price = metadata["price"]
+        if type(price) is int:
+            return f"${price}"
+        else:
+            return price
 
-    return -1
+    return "PRICE HERE"
 
 
 def get_extras(item) -> str:
     metadata = parse_metadata(item)
-    if 'extras' in metadata:
-        return metadata['extras']
+    if "extras" in metadata:
+        return metadata["extras"]
 
-    return ''
+    return ""
 
 
-def to_game_dict(item):
+def to_game_dict(item) -> dict:
     if should_ignore(item):
         return None
 
     name = item[0].text
     attrs = item.attrib
-    url = formaturl.format(id=attrs["objectid"])
-    return {'name': item[0].text,
-            'link': getlink(item),
-            'condition': getcondition(item),
-            'price': f'${get_price(item)}',
-            'extras': get_extras(item),
-            'notes': []}
+    url = GAME_URL.format(id=attrs["objectid"])
+    return {
+        "name": item[0].text,
+        "link": extract_link(item),
+        "condition": get_condition(item),
+        "price": get_price(item),
+        "extras": get_extras(item),
+        "notes": [],
+    }
 
 
-def transformrow(row):
-    if len(row['notes']) > 0:
-        row['link'] = row['link'] + " Bundle"
-        row['notes'] = 'Bundle includes: ' + ', '.join(row['notes'])
+def dict_to_table_row(row: dict) -> dict:
+    if len(row["notes"]) > 0:
+        row["link"] = row["link"] + " Bundle"
+        row["notes"] = "Bundle includes: " + ", ".join(row["notes"])
     else:
-        row['notes'] = ""
+        row["notes"] = ""
 
-    row['notes'] = row['extras'] + ' ' + row['notes']
+    row["notes"] = row["extras"] + " " + row["notes"]
     return row
 
 
+gamerow = {}
+
 for item in parsed:
     name = item[0].text
-    expansion = [ x for x in gamerow.keys() if name.startswith(x) ]
+    expansion = [x for x in gamerow.keys() if name.startswith(x)]
     if len(expansion) > 0:
-        gamerow[expansion[0]]['notes'].append(getlink(item))
+        gamerow[expansion[0]]["notes"].append(extract_link(item))
     else:
         game_dict = to_game_dict(item)
         if not game_dict:
@@ -128,16 +137,13 @@ for item in parsed:
         gamerow[name] = game_dict
 
 
-print('\n\n---- Generating table below -----------------\n')
+print("\n\n---- Generating table below -----------------\n")
 
 print("Game|Condition|Price (OBO)|Notes")
 print(":--|:--|:--|:--")
-for r in [ formatrow.format(**transformrow(v)) for k, v in gamerow.items() ]:
-    print(r)
+[
+    print("{link}|{condition}|{price}|{notes}".format(**dict_to_table_row(v)))
+    for k, v in gamerow.items()
+]
 
-print('\n---- Done generating table --------------------')
-
-
-
-
-
+print("\n---- Done generating table --------------------")
